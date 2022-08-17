e38514a3b75206a57e0bdac24de12ab7275c6778
VerseVent <mike.82pronka.27a@gmail.com>
2022-08-17 20:24:30 +0200

    created component structure

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,84 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <h1 class="todoTitle" :style="{ color: titleColor }">TODO</h1>
+      <TodoForm v-model="currentItem" @addItem="addItem"></TodoForm>
+      <ul class="todoList" v-if="todoList.length">
+        <li
+          class="todoItem"
+          v-for="(todoItem, index) in todoList"
+          :key="'li' + index"
+        >
+          <h2 :class="{ done: todoItem.isDone }" @click="deleteItem(index)">
+            {{ todoItem.task }}
+          </h2>
+          <input type="checkbox" @click="setDone(index)" />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import TodoForm from "./components/TodoForm/TodoForm.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    TodoForm,
+  },
+  data() {
+    return {
+      todoList: [],
+      item: {},
+      currentItem: "",
+      description: "",
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.currentItem.length) {
+        this.item = {
+          task: this.currentItem,
+          isDone: false,
+          description: this.description,
+        };
+        this.todoList.push(this.item);
+        this.item = {};
+        this.currentItem = "";
+      }
+      return;
+    },
+    setDone(id) {
+      this.todoList[id].isDone = !this.todoList[id].isDone;
+    },
+    deleteItem(id) {
+      this.todoList = this.todoList.filter((item, i) => i !== id);
+    },
+  },
+  computed: {
+    titleColor() {
+      const doneItemsLength = this.todoList.filter(
+        (item) => item.isDone
+      ).length;
+      const isNotDoneItemsLength = this.todoList.filter(
+        (item) => !item.isDone
+      ).length;
+      if (doneItemsLength === 0 || doneItemsLength < isNotDoneItemsLength)
+        return "red";
+      if (
+        doneItemsLength >= isNotDoneItemsLength &&
+        doneItemsLength !== this.todoList.length
+      )
+        return "yellow";
+      return "green";
+    },
+  },
+};
 </script>
 
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap";
+@import "./styles/index.scss";
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
---
+++ src/components/TodoForm/TodoForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form @submit.prevent="emitAddItem()">
+    <input
+      type="text"
+      v-on:input="$emit('input', $event.target.value)"
+      ref="itemLabel"
+    />
+    <textarea name="" id="" cols="30" rows="10"></textarea>
+    <button class="addTaskBtn" type="submit" @keydown.enter="emitAddItem()">
+      Add task
+    </button>
+  </form>
+</template>
+
+<script>
+export default {
+  methods: {
+    emitAddItem() {
+      this.$emit("addItem");
+      this.$refs["itemLabel"].value = "";
+    },
+  },
+};
+</script>
+
+<style>
+</style>
