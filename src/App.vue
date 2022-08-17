e021de8ce7c4fdf8403c417e34bf0e31e72e00a7
VerseVent <mike.82pronka.27a@gmail.com>
2022-08-17 20:29:02 +0200

    create todolist

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,79 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="container">
+      <h1 class="todoTitle" :style="{ color: titleColor }">TODO</h1>
+      <form @submit.prevent="addItem">
+        <input type="text" v-model="currentItem" />
+        <button class="addTaskBtn" type="submit" @keydown.enter="addItem">
+          Add task
+        </button>
+      </form>
+      <ul class="todoList" v-if="todoList.length">
+        <li
+          class="todoItem"
+          v-for="(todoItem, index) in todoList"
+          :key="'li' + index"
+        >
+          <h2 :class="{ done: todoItem.isDone }" @click="deleteItem(index)">
+            {{ todoItem.task }}
+          </h2>
+          <input type="checkbox" @click="setDone(index)" />
+        </li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      todoList: [],
+      item: {},
+      currentItem: "",
+    };
+  },
+  methods: {
+    addItem() {
+      if (this.currentItem.length) {
+        this.item = {
+          task: this.currentItem,
+          isDone: false,
+        };
+        this.todoList.push(this.item);
+        this.item = {};
+        this.currentItem = "";
+      }
+      return;
+    },
+    setDone(id) {
+      this.todoList[id].isDone = !this.todoList[id].isDone;
+    },
+    deleteItem(id) {
+      this.todoList = this.todoList.filter((item, i) => i !== id);
+    },
+  },
+  computed: {
+    titleColor() {
+      if (this.todoList.filter((item) => item.isDone).length === 0)
+        return "red";
+      if (
+        this.todoList.filter((item) => item.isDone).length >=
+          this.todoList.filter((item) => !item.isDone).length &&
+        this.todoList.filter((item) => item.isDone).length !==
+          this.todoList.length
+      )
+        return "yellow";
+      return "green";
+    },
+  },
+};
 </script>
 
 <style lang="scss">
 @import "~bootstrap/scss/bootstrap";
+@import "./styles/index.scss";
 
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
