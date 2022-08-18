1fe2247e663154893e1439f4b7e64924d4a9dc93
Yuliia-Sydorovych <yuliiasydorovych@gmail.com>
2022-08-18 11:57:02 +0300

    Create ToDo component; Implement ability to Add, Cross and Delete items; Show list of items; Create abillity to change font-color of title depending on number of crossed out items; Add simple styles.

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id='app'>
+    <ToDo msg='I want to do...'/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ToDo from './components/ToDo.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ToDo
   }
 }
 </script>
 
-<style lang="scss">
-@import "~bootstrap/scss/bootstrap";
-
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style>
+  html {
+    height: 100%;
+    width: 100%;
+    background-image: linear-gradient(to bottom, #af85b8, #bca1ca, #cbbdda, #ded9e8, #f4f4f4);
+  }
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 </style>
---
+++ src/components/ToDo.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="todo">
+    <h1 :style="{ color: fontColor }">
+      {{ msg }}</h1>
+    <input type="text" placeholder="Enter text..." v-model.trim="currentValue" class="input-current" />
+    <button @click="addItem" class="btn-add">ADD</button>
+    <ul class="ul-list">
+      <li v-for="(item, index) in items" :key="item.id" class="li-item">
+        <button @click="removeItem(index, item)"
+          :class="{
+            'btn-remove': item,
+            'li-cross': item.done===true}">
+          {{item.title}}
+        </button>
+        <input type="checkbox" @click="crossItem(item)" v-model="item.done" />
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "ToDo",
+    props: {
+      msg: String
+    },
+    data () {
+      return {
+        items:[],
+        currentValue:"",
+        count:0,
+        color:""
+      }
+    },
+    methods: {
+      addItem() {
+        this.currentValue?this.items.push({
+          title: this.currentValue,
+          done: false
+        }):false;
+        this.currentValue="";
+      },
+      crossItem(item) {
+        item.done = !item.done;
+        item.done?this.count++:this.count--;
+      },
+      removeItem(index, item) {
+        this.items.splice(index, 1);
+        (this.count!==0&&item.done)?this.count--:false;
+      }
+    },
+    computed: {
+      fontColor() {
+        const result = Math.floor(this.items.length/2);
+        return this.count===0&&this.items.length>0?"red"
+          :this.count>result&&this.count!==this.items.length?"yellow"
+            :this.count===this.items.length&&this.count!==0?"green"
+              :"rgb(17, 1, 34)"
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+  @mixin btn-style {
+    font-weight: 700;
+    color: white;
+    background-color: rgb(70, 42, 82);
+    border: 1px solid rgb(17, 1, 34);
+    border-radius: 2px;
+  }
+  .ul-list {
+    list-style-type: none;
+    padding: 0;
+  }
+  .li-cross {
+    text-decoration: line-through;
+  }
+  input[type=text] {
+    width: 300px;
+    height: 30px;
+    border: 1px solid rgb(17, 1, 34);
+    border-radius: 2px;
+  }
+  .btn-add {
+    height: 34px;
+    width: 45px;
+    @include btn-style;
+  }
+  .btn-remove {
+    width: 250px;
+    height: 30px;
+    @include btn-style;
+  }
+  input[type=checkbox] {
+    height: 31px;
+    width: 45px;
+    margin: 0;
+  }
+  .li-item {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items:center;
+    margin-bottom: 5px;
+  }
+</style>
