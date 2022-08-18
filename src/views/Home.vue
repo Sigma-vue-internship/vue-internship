f900847ad56064bfb3638196187231f2352fb37f
Roman Golubov <roman.golubov@sigma.software>
2022-08-18 17:43:38 +0300

    router example

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <img width="200" alt="Vue logo" src="../assets/logo.jpeg">
+    <h2>Home Page</h2>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  }
+}
+</script>
