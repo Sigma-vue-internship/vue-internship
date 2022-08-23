67e7591adad69fde02b323130cf880516e7a145b
Yuliia-Sydorovych <yuliiasydorovych@gmail.com>
2022-08-23 19:48:37 +0300

    Install Vuex, Vue Axios, Vue Bootstrap, SASS

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import store from './store'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import { BootstrapVue } from 'bootstrap-vue'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue, VueAxios, axios)
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  store,
+  render: h => h(App)
 }).$mount('#app')
