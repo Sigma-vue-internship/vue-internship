e40167278677600a8a59cc73d6b8d8aab1a43cf7
VerseVent <mike.82pronka.27a@gmail.com>
2022-08-24 00:36:33 +0200

    FEATURE create navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,15 @@
 <template>
   <div id="app">
-    <nav><router-link to="/">Home</router-link></nav>
+    <Navbar></Navbar>
     <router-view />
   </div>
 </template>
-
+<script>
+import Navbar from "./components/Navbar.vue";
+export default {
+  components: { Navbar },
+};
+</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
@@ -12,18 +17,5 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <b-nav class="p-2 justify-content-between">
+      <b-nav-item to="/">Home</b-nav-item>
+      <div class="d-flex">
+        <b-nav-item to="/search">Search</b-nav-item>
+        <b-nav-item to="/user/signup">Sign up</b-nav-item>
+      </div>
+    </b-nav>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.nav-link {
+  font-weight: 400;
+  color: rgba(0, 0, 0, 0.747);
+}
+a.nav-link.router-link-exact-active.router-link-active {
+  color: rgb(0, 61, 194);
+}
+</style>
