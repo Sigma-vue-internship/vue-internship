a4c05e6b09a42f00caab8adee4269c504052b4a5
VerseVent <mike.82pronka.27a@gmail.com>
2022-09-06 10:44:47 +0200

    return previous breadCrumbs logic

--- src/components/layout/Breadcrumbs.vue
+++ src/components/layout/Breadcrumbs.vue
@@ -1,11 +1,11 @@
 <template>
-  <b-breadcrumb class="container px-3 py-3" v-if="breadCrumbs.length">
+  <b-breadcrumb class="container px-5 py-3" v-if="breadCrumbs">
     <b-breadcrumb-item
       class="breadcrumb"
       v-for="(crumb, i) in breadCrumbs"
       :key="`${crumb.label} ${i}`"
       :to="crumb.to"
-      :active="i === breadCrumbs.length - 1"
+      :active="i === breadCrumbs.length - 1 ? true : false"
       >{{ crumb.label }}</b-breadcrumb-item
     >
   </b-breadcrumb>
@@ -13,47 +13,18 @@
 
 <script>
 export default {
-  data() {
-    return {
-      breadCrumbs: [],
-      maxCrumbs: 5,
-    };
-  },
-  watch: {
-    $route: {
-      handler({ meta: { breadCrumb }, fullPath }, prevRoute) {
-        if (fullPath === "/") {
-          this.breadCrumbs = [];
-          return;
-        }
-        this.breadCrumbs.forEach((crumb, i) => {
-          if (crumb.to === fullPath) {
-            this.breadCrumbs = this.breadCrumbs.filter(
-              (crumb, _, arr) => arr.indexOf(crumb) < i
-            );
-            return;
-          }
-        });
-        if (
-          prevRoute.fullPath === "/" &&
-          fullPath !== "/" &&
-          prevRoute.meta.breadCrumb
-        ) {
-          const prevRouteLabel = prevRoute.meta.breadCrumb[0].label;
-          const prevRoutePath = prevRoute.fullPath;
-          this.breadCrumbs.push({ label: prevRouteLabel, to: prevRoutePath });
-        }
-        this.breadCrumbs.push({ label: breadCrumb[0].label, to: fullPath });
-        if (this.breadCrumbs.length === this.maxCrumbs)
-          this.breadCrumbs.shift();
-      },
-      deep: true,
+  computed: {
+    breadCrumbs() {
+      if (typeof this.$route.meta.breadCrumb === "function") {
+        return this.$route.meta.breadCrumb.call(this, this.$route);
+      }
+      return this.$route.meta.breadCrumb;
     },
   },
 };
 </script>
 <style lang="scss" scoped>
-@import "../../assets/scss/variables.scss";
+@import "../assets/scss/variables.scss";
 .breadcrumb {
   --bs-breadcrumb-margin-bottom: 0;
   --bs-breadcrumb-divider-color: 0;
@@ -66,9 +37,7 @@ export default {
     }
   }
   .active {
-    span {
-      color: $lightGreen;
-    }
+    color: $lightGreen;
   }
 }
 </style>
